3acad5188c0afaa90053b8c14a5744099845844d
Antoine Lavallée <antoine.lavallee@flyable.dev>
2022-01-14 12:16:36 -0500

    fix condition conversion for bool type

--- flyable/code_gen/cond.py
+++ flyable/code_gen/cond.py
@@ -4,6 +4,7 @@ import flyable.data.lang_type as lang_type
 import flyable.code_gen.code_type as code_type
 import flyable.code_gen.caller as caller
 import flyable.data.type_hint as hint
+import flyable.code_gen.debug as debug
 import flyable.code_gen.ref_counter as ref_counter
 
 
@@ -11,6 +12,7 @@ def value_to_cond(visitor, value_type, value):
     """
     Convert the value into an integer usable into a conditional branching
     """
+
     code_gen, builder = visitor.get_code_gen(), visitor.get_builder()
     if value_type.is_int() or value_type.is_bool():
         return value_type, value  # int and bool doesn't need conversion
@@ -46,22 +48,24 @@ def test_obj_true(visitor, value_type, value):
     test_true_with_call_block = builder.create_block()
     builder.cond_br(is_true, true_block, test_true_with_call_block)
 
-    test_true_with_number_protocol_block = builder.create_block()
-
     builder.set_insert_block(true_block)
     builder.store(builder.const_int1(True), result)
     continue_block = builder.create_block()
     builder.br(continue_block)
 
     builder.set_insert_block(test_true_with_call_block)
+
     cond_type, cond_value = caller.call_obj(visitor, "__bool__", value, value_type, [], [])
+
     false_block = builder.create_block()
     if cond_type.is_python_obj() or cond_type.is_collection() or cond_type.is_obj():
         is_true_2 = builder.eq(cond_value, true_value)
         ref_counter.ref_decr_incr(visitor, cond_type, cond_value)
         builder.cond_br(is_true_2, true_block, false_block)
-    elif cond_type.is_bool() or cond_type.is_int():
+    elif cond_type.is_bool():
         builder.cond_br(cond_value, true_block, false_block)
+    elif cond_type.is_int():
+        builder.cond_br(builder.int_cast(cond_value, code_type.get_int1()), true_block, false_block)
     else:
         error_str = "__bool__ should return bool, returned '" + cond_type.to_str(code_gen.get_data()) + "' instead"
         visitor.get_parser().throw_error(error_str, visitor.get_current_node().line_no, 0)
