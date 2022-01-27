9b5164c2807f6a72cbd31ae8ae6436180456ded0
Mathis Laroche <ecomath360@gmail.com>
2022-01-27 11:46:41 -0500

    DebugFlags improvements
    # It is now possible to set value to debug_flags
    # Adding the function do_if_debug that calls a function if a flag is enabled
    # Adding parameter condition in do_if_debug and in value_if_debug. This parameter, when set, is a lambda that tests the value of the flag

--- flyable/debug/debug_flags.py
+++ flyable/debug/debug_flags.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 from enum import Enum, auto
+from typing import Callable
 
 
 class DebugFlags(Enum):
@@ -10,6 +11,10 @@ class DebugFlags(Enum):
     SHOW_OUTPUT_BUILDER = auto()
     SHOW_OPCODE_ON_EXEC = auto()
     PRINT_FUNC_IMPL = auto()
+    PRINT_INT64 = auto()
+
+    STEP_LEVEL = 1
+    """STEP_LEVEL: Value range between 0 and 4"""
 
     @classmethod
     def enable_debug_flags(cls, *debug_flags: DebugFlags):
@@ -44,21 +49,59 @@ class DebugFlags(Enum):
         return f"{self.name}={self.is_enabled}"
 
 
-def value_if_debug(normal_value, debug_value, flag: DebugFlags):
-    """Convenience function that return the normal value if the flag is not enabled and the debug value if it is
+def get_flag_value(flag: DebugFlags):
+    return flag.value
 
-    Args:
-        normal_value (? extends T): Value to be returned if the debug flag is not enabled
-        debug_value (? extends T): Value to be returned if the debug flag is enabled
-        flag (DebugFlags): Flag to inspect
 
-    Returns:
-        T: Either the normal value or the debug value, depending if the flag is enabled
+def value_if_debug(
+        normal_value, debug_value, flag: DebugFlags, condition: Callable = None
+):
+    """Convenience function that returns the normal value if the flag is not enabled or the flag's value doesn't meet
+    the requirements and returns the debug value if it is enabled and its value meets the requirements
+
+    Example:
+        value_if_debug("hello", "world", DebugFlags.STEP_LEVEL, lambda level: level >= 2)
+
+        value_if_debug("foo", "bar", DebugFlags.SHOW_VISIT_AST)
+
+    :param normal_value: (? extends T): Value to be returned if the debug flag is not enabled
+    :param debug_value: (? extends T): Value to be returned if the debug flag is enabled
+    :param flag: (DebugFlags): Flag to inspect
+    :param condition: (Callable): predicate condition for the flag value (optionnal)
+
+    :returns: Either the normal value or the debug value, depending if the flag is enabled
     """
-    if flag.is_enabled:
+    if flag.is_enabled and (condition is None or condition(flag.value)):
         return debug_value
     else:
         return normal_value
+
+
+def do_if_debug(
+        func: Callable,
+        flag: DebugFlags,
+        args: tuple = (),
+        kwargs: dict = None,
+        condition: Callable = None,
+):
+    """Convenience function that calls a function if the flag is enabled and the flag's value meets the requirements
+
+    Example:
+        do_if_debug(print, args=("hello", "world"), kwargs={"sep": "\\\\n"}, flag=DebugFlags.STEP_LEVEL,
+        condition=lambda level: level >= 2)
+        do_if_debug(input, args=("Enter your message",) DebugFlags.SHOW_VISIT_AST)
+
+    :param func: (Callable): function to call if the flag is enabled
+    :param args: (tuple): args to the function
+    :param kwargs: (dict): kwargs to the function
+    :param flag: (DebugFlags): Flag to inspect
+    :param condition: (Callable): predicate condition for the flag value (optionnal)
+
+    :returns: Either the normal value or the debug value, depending if the flag is enabled
+    """
+    if flag.is_enabled and (condition is None or condition(flag.value)):
+        kwargs = kwargs if kwargs is not None else {}
+        return func(*args, **kwargs)
 
 
 def debug_flag_enabled(flag: DebugFlags):
