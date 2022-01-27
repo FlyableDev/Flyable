3003773a6dba2ee651dbdf8a38fb5aa23050688c
Mathis Laroche <ecomath360@gmail.com>
2022-01-27 12:12:59 -0500

    Improvements to the utils.py module
    # Steps are now stored in a stack, so they can be embedded
    # Steps can now be interrupted with a reason as well as ended (with possibility to cascade the interruption)

--- flyable/tool/utils.py
+++ flyable/tool/utils.py
@@ -6,8 +6,9 @@ from typing import Literal, Callable, Any, Iterable
 @dataclass
 class Step:
     name: str
-    status: Literal["idle", "ongoing", "completed",
-                    "interrupted"] = field(default="idle")
+    status: Literal["idle", "ongoing", "completed", "interrupted"] = field(
+        default="idle"
+    )
 
     def start(self):
         print(f"[START] {self.name}...")
@@ -17,27 +18,73 @@ class Step:
         print(f"[COMPLETE] {self.name}")
         self.status = "completed"
 
-    def interrupted(self):
-        print(f"[INTERRUPTED] {self.name}")
+    def interrupt(self, reason: str):
+        print(f"[INTERRUPTED] {self.name} because {reason}")
         self.status = "interrupted"
 
 
-__current_step: Step = None
+__current_step: list[Step] = []
+
+
+def get_step(step_name: str):
+    """
+    returns the step with the matching name or None if they are no step with a matching name
+    """
+    return find_first(lambda step_: step_.name == step_name, __current_step)
+
+
+def get_step_idx(step_name: str):
+    """
+    returns the step index with the matching name or None if they are no step with a matching name
+    """
+    return find_first_idx(lambda step_: step_.name == step_name, __current_step)
+
+
+def add_step(step_name: str, start: bool = True):
+    step = Step(name=step_name)
+    __current_step.append(step)
+    if start:
+        step.start()
 
 
 def start_step(step_name: str):
-    global __current_step
-    __current_step = Step(name=step_name)
-    __current_step.start()
+    step = get_step(step_name)
+    step and step.start()
 
 
-def end_step():
-    global __current_step
-    __current_step and __current_step.end()
-    __current_step = None
+def interrupt_step(reason: str, cascade: bool = False, step_name: str = None):
+    """
+    Interrupts a certain step or the current step. If cascade is set to True, also interrupts all the step
+    above the step interrupted
+
+    :param reason: the reason for the interruption
+    :param cascade: if the interruption should interrupt all step over this one
+    :param step_name: the name of the interrupted step (keep None for the current step)
+    """
+    if not __current_step:
+        return
+
+    step_idx = -1 if step_name is None else get_step_idx(step_name)
+    if step_idx is None:
+        return
+    __current_step.pop(step_idx).interrupt(reason)
+    while cascade and abs(step_idx) < len(__current_step):
+        step = __current_step.pop(step_idx)
+        step.interrupt(f"Step {step.name} was interrupted")
+
+
+def end_step(step_name: str = None):
+    if not __current_step:
+        return
+    if step_name is None:
+        __current_step.pop().end()
+        return
+    step = get_step(step_name)
+    step and step.end()
 
 
 # ###################################### list utils ###################################### #
+
 
 def find_first(predicate: Callable[[Any], bool], iterable: Iterable):
     """Returns the first value that matches in the iterable"""
