f6b4c94af5966a93501501c600b5b3b2fa4288a0
Mathis Laroche <ecomath360@gmail.com>
2022-02-06 15:50:26 -0500

    Added the content of generated_script to the gitignore

--- tests/unit_tests/conditions/generated_scripts/body_conditions.py
+++
@@ -1,6 +0,0 @@
-if False:
-    print(1)
-elif False:
-    print(2)
-else:
-    print(3)
--- tests/unit_tests/str/generated_scripts/body_str.py
+++
@@ -1,4 +0,0 @@
-litteral_str = ""
-litteral_str_by_method = str()
-print(litteral_str == litteral_str_by_method)
-# print(litteral_str == litteral_str_by_method)
