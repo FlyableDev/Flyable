ed192e8f12539a3f2be2f65d7555cce4784be98b
Dayton J Allen <dayton.allen@flyable.dev>
2022-06-06 11:03:46 -0700

    move build files to module

--- setup.py
+++ setup.py
@@ -1,37 +1,11 @@
 import setuptools
-import platform
-
-
-def get_lib_folder_name() -> str:
-    system = platform.system()
-    if system == "Windows":
-        return "win64"
-
-    if system == "Darwin":
-        return "macos-arm64"
-
-    if system == "Linux":
-        return "linux64"
-
-
-def get_extension() -> str:
-    system = platform.system()
-    if system == "Windows":
-        return "dll"
-
-    if system == "Darwin":
-        return "dylib"
-
-    if system == "Linux":
-        return "so"
-
 
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
     name="flyable",
-    version="0.0.1",
+    version="0.0.1.1",
     author="FlyableDev",
     author_email="contact@flyable.dev",
     description="Flyable is a Python compiler that generates optimized native code to improve the performance of Python workloads.",
@@ -50,6 +24,6 @@ setuptools.setup(
     packages=setuptools.find_packages(where="src"),
     python_requires=">=3.11",
     package_data={
-        "": [f"dyn_lib/{get_lib_folder_name()}/libflyableengine.{get_extension()}"]
+        "": [f"dyn_lib/macos-arm64/BugpointPasses.dylib"]
     },
 )
